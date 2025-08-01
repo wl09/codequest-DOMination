db0fee4f3fc38f92b3a2c5472f49168fe759f647
Wema <150550352+wl09@users.noreply.github.com>
2025-08-01 18:11:24 -0400

    created quiz questions in js file

---
+++ codequest.js
@@ -0,0 +1,27 @@
+const quizQuestions = [
+  {
+    question: "Which company developed JavaScript?",
+    options: ["Microsoft", "Netscape", "Google", "Apple"],
+    correctAnswer: "Netscape"
+  },
+  {
+    question: "Which symbol is used for comments in JavaScript?",
+    options: ["//", "/* */", "#", "<!-- -->"],
+    correctAnswer: "//"
+  },
+  {
+    question: "Which keyword is used to declare a variable in JavaScript?",
+    options: ["var", "let", "const", "All of the above"],
+    correctAnswer: "All of the above"
+  },
+  {
+    question: "What will the expression `typeof 42` return?",
+    options: ["number", "string", "boolean", "undefined"],
+    correctAnswer: "number"
+  },
+  {
+    question: "Which method is used to add an element to the end of an array?",
+    options: [".push()", ".pop()", ".shift()", ".unshift()"],
+    correctAnswer: ".push()"
+  }
+];
